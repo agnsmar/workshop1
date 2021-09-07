802aa7ebbdb7348ce28b845653d0d66ae675ca08
Denni Wernersson <dw222fy@student.lnu.se>
2021-09-07 11:31:04 +0200

    Session 2 changes

--- codeMetrics.js
+++ codeMetrics.js
@@ -3,17 +3,32 @@ export class codeMetrics {
     this.codeToAnalyze = codeToAnalyze
   }
 
-  countNewlines() {
-    // console.log('before', this.codeToAnalyze)
-    const newlineCount = this.codeToAnalyze.split(/\n/).length
-
-    console.log('newline count:', newlineCount)
-
+  matcher(str, regex) {
+    return str.match(regex)
   }
 
-  countNewlinesNoWhitespace() {
-    const whitespace = this.codeToAnalyze.split(/(?:[\t ]*(?:\r?\n|\r))+/).length
-    
-    console.log('whitespace count:', whitespace)
+  countNewlines() {
+    const newlineCount = this.codeToAnalyze.split(/\n/).length
+
+    return newlineCount
+  }
+
+  countNewlinesNoWhitelines() {
+    return this.countNewlines() - this.countWhitelines()
+  }
+
+  countWhitelines() {
+    let regexExample = /[\r\n]+/g
+    const whiteLineArray = []
+
+    const whiteCount = this.matcher(this.codeToAnalyze, regexExample)
+
+    for (let i = 0; i < whiteCount.length; i++) {
+      if (whiteCount[i] === '\r\n\r\n') {
+        whiteLineArray.push(whiteCount[i])
+      }
+    }
+
+    return whiteLineArray.length
   }
 }
